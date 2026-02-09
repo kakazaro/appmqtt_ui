eecfeeffe857f6f1cddaeb2b4c69a5be3fe215c8
quanle <vanquanbk84@gmail.com>
2026-02-09 17:48:59 +0700

    update 1.4.5 - update UI

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet, View} from 'react-native';
 import { Provider } from 'react-native-paper';
 import MainScreen from './screen/mainScreen';
 import UpdateWrap from './component/updateWrap';
@@ -19,11 +18,11 @@ export default function App() {
             <SiteProvider>
                 <Provider>
                     <RootSiblingParent>
-                        <SafeAreaView style={styles.container}>
+                        <View style={styles.container}>
                             <UpdateWrap>
                                 <MainScreen/>
                             </UpdateWrap>
-                        </SafeAreaView>
+                        </View>
                     </RootSiblingParent>
                 </Provider>
             </SiteProvider>
